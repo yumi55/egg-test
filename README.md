29b145aa5ee0a610e68b082a2e3de496c06d0714
zhangyuling2 <yuling.zhang@runwise.co>
2024-02-29 17:00:14 +0800

    项目搭建

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-# egg-teste
+# test
 
-git init
-git add README.md
+
+
+## QuickStart
+
+<!-- add docs here for user -->
+
+see [egg docs][egg] for more detail.
+
+### Development
+
+```bash
+npm i
+npm run dev
+open http://localhost:7001/
+```
+
+### Deploy
+
+```bash
+npm start
+npm stop
+```
+
+### npm scripts
+
+- Use `npm run lint` to check code style.
+- Use `npm test` to run unit test.
+
+[egg]: https://eggjs.org
---
+++ config/config.default.js
@@ -0,0 +1,37 @@
+/* eslint valid-jsdoc: "off" */
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1709196804769_4720';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mongoose = {
+    client: {
+      url: 'mongodb://127.0.0.1/yumi-blog',
+      options: {},
+      // mongoose global plugins, expected a function or an array of function and options
+      plugins: [],
+    },
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
